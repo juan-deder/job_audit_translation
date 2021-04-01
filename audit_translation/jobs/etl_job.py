1965c4496cf25307fb8b36f892e123aaf0d73c84
Juan Fernando <jfdeder@gmail.com>
2021-04-01 16:07:07 -0500

    Tests unitarios arreglados y extracción de homologación agregada

--- audit_translation/jobs/etl_job.py
+++ audit_translation/jobs/etl_job.py
@@ -1,10 +1,10 @@
-from datetime import date
-from audit_translation.tests.test_audit_translation import  \
-    AuditTranslationTestCase
 import awswrangler as wr
+import pandas as pd
+from datetime import date
 from audit_translation.config import Config
-from audit_translation.config.constants import DATA_SOURCE, \
-    CATEGORY_EXTRACT_FIELDS, ASSOCIATION_ENTITIES, TRANSLATE_COLS, VALUE_COLS, COLUMN_COLS, TABLE_COLS, PARTITION_COLS, PATH, INPUT_PATH
+from audit_translation.config.constants import CATEGORY_EXTRACT_FIELDS, \
+    ASSOCIATION_ENTITIES, TRANSLATE_COLS, VALUE_COLS, COLUMN_COLS, TABLE_COLS, \
+    PARTITION_COLS, PATH, INPUT_PATH
 
 config = Config()
 
@@ -72,17 +72,30 @@ def translate(mapping, index, default):
     else:
         if (index[:2] in mapping.index and len(mapping.loc[index[:2]]) > 1) \
                 or index[2] == '':
-            #print('Translation does not exist for index: ' + '/'.join(index))
-            pass
+            print('Translation does not exist for index: ' + '/'.join(index))
         return default
 
 
 def extract():
     year, month, day = [str(int(x)) for x in date.today().isoformat().split('-')]
-    return wr.s3.read_parquet(f's3://{getattr(config, INPUT_PATH)}/data/audit/', dataset=True, partition_filter=lambda x: x['year'] == year and x['month'] == month and x['day'] == '30')
+    return wr.s3.read_parquet(
+        f's3://{getattr(config, INPUT_PATH)}/data/audit/', dataset=True,
+        partition_filter=lambda x: x['year'] == year and x['month'] == month and x['day'] == day)
 
 
-def transform(records, mapping=TRANSLATION):
+def extract_mapping():
+    con = wr.redshift.connect('CONFIGURAR UNA AWS GLUE CONNECTION HACIA REDSHIFT Y REEMPLAZAR ESTE STRING CON EL NOMBRE DE LA CONEXIÓN. https://docs.aws.amazon.com/glue/latest/dg/console-connections.html?icmpid=docs_glue_console')
+    with con.cursor() as cursor:
+        cursor.execute(
+            'select column_name_coh as column, table_name_coh as table, '
+            'value_coh as value, translation_coh as translation '
+            'from audit_test.config_homolagacion_coh')
+        mapping = [dict(row) for row in cursor.fetchall()]
+    con.close()
+    return pd.DataFrame(mapping)
+
+
+def transform(records, mapping):
     mapping.set_index(['table', 'column', 'value'], inplace=True)
     return records.apply(apply_func, 1, result_type='expand', mapping=mapping)
 
@@ -94,7 +107,8 @@ def load(df):
 
 def main():
     records = extract()
-    df = transform(records)
+    mapping = extract_mapping()
+    df = transform(records, mapping)
     load(df)
 
 
--- audit_translation/tests/test_audit_translation.py
+++ audit_translation/tests/test_audit_translation.py
@@ -1,7 +1,18 @@
 import pandas as pd
+from json import dumps
 from unittest import TestCase
 from datetime import datetime
 from audit_translation.jobs.etl_job import transform
+
+COMMON_FIELDS = {
+    'timestamp': datetime(2020, 12, 12),
+    'owner': 'PRIME',
+    'year': 2020,
+    'month': 12,
+    'day': 12,
+    'user_id': 'sameUser',
+    'auditTrackId': f'2000-MS',
+}
 
 
 class AuditTranslationTestCase(TestCase):
@@ -27,12 +38,6 @@ class AuditTranslationTestCase(TestCase):
         {'table': 'ms_relation_device_rde', 'column': 'id_metering_type_dev', 'value': '20', 'translation': 'Backup'}
     ])
 
-    COMMON_INPUT_FIELDS = {
-        'sourceIp': f'1.1.1.1',
-        'categoryName': 'Audit',
-        'auditTrackId': f'2000-MS'
-    }
-
     def test_regular_audit_translation(self):
         records = self.craft_audit([{
             'entityIdName1': 'number_spo',
@@ -42,13 +47,11 @@ class AuditTranslationTestCase(TestCase):
             'columnName': 'id_metering_type_dev',
             'oldValue': '19',
             'newValue': '20',
-            'dataOperation': 'UPDATE',
             'entitySource': 'ms_relation_device_rde',
-        } for i in range(10)], 10)
+        } for i in range(10)])
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
-            'timestamp': datetime(2020, 12, 12),
-            'user_id': 'sameUser',
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'entityId1': f'SOC_V1_PAP_{i}',
             'entityId2': f'DEV_V1_PAP_{i}',
             'columnName': 'Tipo de medida',
@@ -59,7 +62,7 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': 'Dispositivo',
             'dataOperation2': 'Actualizar'} for i in range(10)
         ])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     def test_relation_translation(self):
         records = self.craft_audit({
@@ -70,11 +73,11 @@ class AuditTranslationTestCase(TestCase):
             'columnName': 'id_metering_type_dev',
             'oldValue': '19',
             'newValue': '20',
-            'dataOperation': 'UPDATE',
             'entitySource': 'ms_relation_device_rde',
         })
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'columnName': 'Tipo de medida',
             'dataOperation': '',
             'dataOperation2': 'Actualizar',
@@ -84,10 +87,9 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': 'Dispositivo',
             'newValue': 'Backup',
             'oldValue': 'Main',
-            'timestamp': datetime(2020, 12, 12),
             'user_id': 'sameUser'
         }])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     def test_description_HES_audit(self):
         records = self.craft_audit({
@@ -98,11 +100,11 @@ class AuditTranslationTestCase(TestCase):
             'columnName': 'desc_int',
             'oldValue': 'NEWDES---------------7',
             'newValue': 'NEWDES---------------8',
-            'dataOperation': 'UPDATE',
             'entitySource': 'ps_interoperability_int',
         })
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'columnName': 'Descripción',
             'dataOperation': 'Actualizar',
             'dataOperation2': '',
@@ -112,10 +114,9 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': '',
             'newValue': 'NEWDES---------------8',
             'oldValue': 'NEWDES---------------7',
-            'timestamp': datetime(2020, 12, 12),
             'user_id': 'sameUser'
         }])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     def test_password_HES_audit(self):
         records = self.craft_audit({
@@ -126,11 +127,11 @@ class AuditTranslationTestCase(TestCase):
             'columnName': 'isPassword',
             'oldValue': 'true',
             'newValue': 'false',
-            'dataOperation': 'UPDATE',
             'entitySource': 'ps_interoperability_int',
         })
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'columnName': 'URL Servicio Web - Es contraseña',
             'dataOperation': 'Actualizar',
             'dataOperation2': '',
@@ -140,15 +141,13 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': '',
             'newValue': 'No',
             'oldValue': 'Sí',
-            'timestamp': datetime(2020, 12, 12),
             'user_id': 'sameUser'
         }])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     def test_plain_value_HES_audit(self):
         records = self.craft_audit({
             'columnName': 'value',
-            'dataOperation': 'UPDATE',
             'entityId1': 'juan - CONEXION4',
             'entityId2': '461',
             'entityIdName1': 'name_int - conexion_name_int',
@@ -158,7 +157,8 @@ class AuditTranslationTestCase(TestCase):
             'oldValue': 'q12',
         })
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'columnName': 'URL Servicio Web - Valor',
             'dataOperation': 'Actualizar',
             'dataOperation2': '',
@@ -168,15 +168,12 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': '',
             'newValue': 'q13',
             'oldValue': 'q12',
-            'timestamp': datetime(2020, 12, 12),
-            'user_id': 'sameUser',
         }])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     def test_domain_value_HES_audit(self):
         records = self.craft_audit({
             'columnName': 'value',
-            'dataOperation': 'UPDATE',
             'entityId1': 'juan - CONEXION4',
             'entityId2': '733',
             'entityIdName1': 'name_int - conexion_name_int',
@@ -186,7 +183,8 @@ class AuditTranslationTestCase(TestCase):
             'oldValue': '30',
         })
         df = transform(records, self.TRANSLATION.copy())
-        expected = pd.DataFrame([{**self.COMMON_INPUT_FIELDS,
+        correct = pd.DataFrame([{
+            **COMMON_FIELDS,
             'columnName': 'Estándar - Valor',
             'dataOperation': 'Actualizar',
             'dataOperation2': '',
@@ -196,27 +194,23 @@ class AuditTranslationTestCase(TestCase):
             'entitySource2': '',
             'newValue': 'Multispeak v5.0',
             'oldValue': 'AMRDef v6.0',
-            'timestamp': datetime(2020, 12, 12),
-            'user_id': 'sameUser',
         }])
-        self.assertTrue(df.equals(expected))
+        self.assertTrue(df.equals(correct[sorted(correct.columns.to_list())]))
 
     @classmethod
-    def craft_audit(cls, category, n=1):
-        audit = {
+    def craft_audit(cls, category):
+        return pd.DataFrame([{
             'transaction_id': 1,
-            'user_id': 'sameUser',
-            'timestamp': datetime(2020, 12, 12),
             'message': 'Updated entity',
             'log_level': 'WARN',
             'owner_id': 'PRIME',
             'source_id': 'PlatformSettings',
             'log_type': 'PAP-LOG-auditory',
-            'category': {**cls.COMMON_INPUT_FIELDS, **category},
-            'owner': 'PRIME',
-            'year': 2020,
-            'month': 12,
-            'day': 12
-        }
-        audit = audit if n > 1 else [audit]
-        return pd.DataFrame(audit)
+            'category': dumps({
+                'sourceIp': f'1.1.1.1',
+                'categoryName': 'Audit',
+                'dataOperation': 'UPDATE',
+                'auditTrackId': f'2000-MS',
+                **c}),
+            **COMMON_FIELDS
+        } for c in ([category] if isinstance(category, dict) else category)])
